70fad9e18b0ba611ca29fa836e798107e259b1ce
Mohit Tiwari <mt0988145@gmail.com>
2026-03-11 23:03:14 +0530

    updating db

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/extension'
+import { WithAccelerateOptions } from '@prisma/extension-accelerate'
 
 const app = new Hono()
-app.post('api/v1/signup',(c)=>c.text("what is this "))
+app.post('api/v1/signup',(c)=>{
+    const prisma = new PrismaClient({
+        datasourceUrl:env.DATABASE_URL,
+    }).$extends
+})
 app.post('api/v1/signin',(c)=>c.text("what is this "))
 app.post('api/v1/blog',(c)=>c.text("what is this "))
 app.put('api/v1/blog',(c)=>c.text("what is this "))
